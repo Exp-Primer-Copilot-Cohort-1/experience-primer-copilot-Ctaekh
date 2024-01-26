6a50dc81c0a46ed1c0d1c8d1a835cf2deb124c9c
Ctaekh <96819320+Ctaekh@users.noreply.github.com>
2024-01-26 23:49:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,9 @@
 const http = require('http');
-
+// Create web server
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+    res.end('Hello, world!');
 });
 
 server.listen(3000, 'localhost', () => {
